d74fdb6798f8d848792df3ce41fb2d2facb1a857
KASSIDY_SHADE <kassie2129@gmail.com>
2019-11-21 11:21:21 -0600

    first steps

---
+++ js.js
@@ -0,0 +1,8 @@
+
+<div onclick="broken()"></div>
+
+div.onclick("click", broken);
+
+function broken() {
+    alert("Sorry, this link isn't working yet. Please try again later.")
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+var clickable = document.getElementsByTagName("div")
+
+broken(clickable, "click",);
+
+
+function broken() {
+    alert("Sorry, this link isn't working yet. Please try again later.")
+}
